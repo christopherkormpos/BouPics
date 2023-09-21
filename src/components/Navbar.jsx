ed3fe53009309ef533f2c357a6aad9948a0f5c4e
unknown <christopher.kormpos@gmail.com>
2023-09-21 18:44:26 +0300

    nav styles and photography page initialization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./Navbar.css";
 
-function CollapsibleExample() {
+function Navigationbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" sticky="top" >
-      <Container>
+    <Navbar collapseOnSelect expand="lg" className="navbar" sticky="top" >
+      <Container className='container'>
         <Navbar.Brand href="/">BouPics</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -15,7 +16,6 @@ function CollapsibleExample() {
             <NavDropdown title="Portfolio" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="/photography">Photography</NavDropdown.Item>
               <NavDropdown.Item href="/videography">Videography</NavDropdown.Item>
-              <NavDropdown.Divider />
             </NavDropdown>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
@@ -25,4 +25,4 @@ function CollapsibleExample() {
   );
 }
 
-export default CollapsibleExample;
+export default Navigationbar;
--- src/components/Pages/Photography.jsx
+++ src/components/Pages/Photography.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Photography.css";
 
 export const Photography = () => {
   return (
-    <div>
-      <h1>Photography</h1>
-    </div>
+    <div className="photography">
+  <div className="image-container weddings">
+    <img src="/images/wedding.jpg" alt="wedding" />
+    <h2>Weddings</h2>
+  </div>
+  <div className="image-container motorsport">
+    <img src="/images/motorsport.jpg" alt="motorsport" />
+    <h2>MotorSport</h2>
+  </div>
+  <div className="image-container landscapes">
+    <img src="/images/lanscape.png" alt="landscape" />
+    <h2>Landscapes</h2>
+  </div>
+</div>
   );
 };
