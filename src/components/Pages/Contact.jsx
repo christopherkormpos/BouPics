09c2acf55c44562f769294c9d9a43536089e9152
unknown <christopher.kormpos@gmail.com>
2023-09-25 14:40:04 +0300

    Home-Contact pages initialization and transitions on photography

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import './Contact.css'
 
 export const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
+      <div className="div-contact">
+        <img src="./images/contact.png" alt="" />
+        <div>
+          <p>Find us here!</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <div>
-      <p>paragrah 1</p>
-      <img src="./images/Photographer.png" alt="" />
-      <img src="./images/Photography.png" alt="" />
-      <p>paragraph 2</p>
+      <div className="content1">
+        <p className="paragraph1">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni reprehenderit dolorum repellat beatae cumque facilis nostrum ex doloribus ad est placeat, asperiores nisi autem, voluptatem earum mollitia incidunt, corporis dolores.
+        </p>
+        <img src="./images/Photographer.png" alt="" className="paragraph1-image" />
+      </div>
+      <div className="content2">
+        <img src="./images/Photography.png" alt="" className="paragraph2-image" />
+        <p className="paragraph2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur voluptatem cumque similique corrupti libero totam culpa modi tempora nesciunt dignissimos. Incidunt fugit dolorum consequuntur corrupti possimus nulla ratione cumque quia.
+        </p>
+      </div>
     </div>
   );
 };
