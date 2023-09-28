f44deec43ce223108c9fd7c3fb4d2633d8c60b25
unknown <christopher.kormpos@gmail.com>
2023-09-28 15:32:20 +0300

    main page paragraphs and photography page changes

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,17 +5,36 @@ export const Home = () => {
   return (
     <div>
       <h1 className="name">George Bougas</h1>
-      <img src="./images/landscape-main.jpg" className= "img-main" alt="" />
+      <img src="./images/landscape-main.jpg" className="img-main" alt="" />
       <div className="content1">
         <p className="paragraph1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni reprehenderit dolorum repellat beatae cumque facilis nostrum ex doloribus ad est placeat, asperiores nisi autem, voluptatem earum mollitia incidunt, corporis dolores.
+          Welcome to Boupics Photography, where moments turn into memories through the lens of creativity.
+          With a passion for visual storytelling, I specialize in capturing life's most cherished experiences.
+          Whether it's the adrenaline rush of Motorsport, the romance of Weddings, or the breathtaking beauty of
+          Landscape, I bring each frame to life with artistry and dedication.
         </p>
         <img src="./images/Photographer.svg" alt="" className="paragraph1-image" />
       </div>
       <div className="content2">
-        <img src="./images/Photography.svg" alt="" className="paragraph2-image" />
+        <div>
         <p className="paragraph2">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur voluptatem cumque similique corrupti libero totam culpa modi tempora nesciunt dignissimos. Incidunt fugit dolorum consequuntur corrupti possimus nulla ratione cumque quia.
+          My portfolio reflects the diverse range of my work, from the high-speed drama of motorsports tracks to
+          the tender moments of wedding ceremonies and the serenity of vast landscapes.Visit my portfolio for a 
+          deeper exploration of my photographic journey and a closer look at the captivating stories waiting to 
+          be unveiled.
+        </p>   
+        <a href="/portfolio" className='button'>
+        <button>Explore My Portfolio</button>
+        </a>
+        </div>
+          <img src="./images/Photography.svg" alt="" className="paragraph2-image" />
+      </div>
+      <div className="content3">
+        <img src="./images/Photography.svg" alt="" className="paragraph3-image" />
+        <p className="paragraph3">
+          Photography serves as my canvas for preserving the fleeting essence of life.
+          Each click of the shutter is an opportunity to encapsulate a moment's emotion, a story's heart, or an
+          experience's magic, revealing the world's beauty in every frame.
         </p>
       </div>
     </div>
--- src/components/Pages/Photography.jsx
+++ src/components/Pages/Photography.jsx
@@ -3,19 +3,33 @@ import "./Photography.css";
 
 export const Photography = () => {
   return (
-    <div className="photography">
-  <div className="image-container weddings">
-    <img src="/images/wedding.jpg" alt="wedding" />
-    <h2>Weddings</h2>
-  </div>
-  <div className="image-container motorsport">
-    <img src="/images/motorsport.jpg" alt="motorsport" />
-    <h2>MotorSport</h2>
-  </div>
-  <div className="image-container landscapes">
-    <img src="/images/lanscape.png" alt="landscape" />
-    <h2>Landscapes</h2>
-  </div>
-</div>
+    <>
+      <div className="photography-intro">
+        <p>As you explore the world through my lens, you'll witness a tapestry of emotions, stories, 
+          and moments frozen in time. Now, it's time to delve deeper into my portfolio—an ever-evolving 
+          collection of my work. These images are not just photographs; they're a visual diary of the incredible 
+          journeys I've embarked upon and the incredible people I've had the privilege to meet. Whether it's the 
+          roar of engines on the racetrack, the tender exchanges of wedding vows, or the serene vistas of 
+          untouched landscapes, each photograph carries a unique narrative waiting to be discovered</p>
+      </div>
+      <div className="photography">
+        <div className="image-container weddings">
+          <img src="/images/wedding.jpg" alt="wedding" />
+          <h2>Weddings</h2>
+        </div>
+        <div className="image-container motorsport">
+          <img src="/images/motorsport.jpg" alt="motorsport" />
+          <h2>MotorSport</h2>
+        </div>
+        <div className="image-container landscapes">
+          <img src="/images/lanscape.png" alt="landscape" />
+          <h2>Landscapes</h2>
+        </div>
+      </div>
+      <div className="photography-outro">
+          <p>Photography is more than images; it's storytelling. 
+            I capture moments that endure, leaving memories etched.</p>
+        </div>
+    </>
   );
 };
